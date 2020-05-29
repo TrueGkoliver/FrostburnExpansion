1c1dbd0adf55e88f0ade82d72945a90e16735407
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-05-29 18:36:34 -0500

    Adding parameters for the Brisk

--- src/main/java/com/teamaurora/frostburnexpansion/common/entity/BriskEntity.java
+++ src/main/java/com/teamaurora/frostburnexpansion/common/entity/BriskEntity.java
@@ -3,13 +3,19 @@ package com.teamaurora.frostburnexpansion.common.entity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.world.World;
 
 public class BriskEntity extends MonsterEntity {
-
+	private static final DataParameter<Integer> STATE = EntityDataManager.createKey(BriskEntity.class, DataSerializers.VARINT);
+	   private static final DataParameter<Boolean> POWERED = EntityDataManager.createKey(BriskEntity.class, DataSerializers.BOOLEAN);
+	   private static final DataParameter<Boolean> IGNITED = EntityDataManager.createKey(BriskEntity.class, DataSerializers.BOOLEAN);
 	public BriskEntity(EntityType<? extends MonsterEntity> entityTypeIn, World worldIn) {
 		super(entityTypeIn, worldIn);
 	}
@@ -21,7 +27,10 @@ public class BriskEntity extends MonsterEntity {
 	
 	@Override
 	protected void registerData() {
-		
+		super.registerData();
+	    this.dataManager.register(STATE, -1);
+	    this.dataManager.register(POWERED, false);
+	    this.dataManager.register(IGNITED, false);
 	}
 
 	@Override
