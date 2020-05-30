6aa15c5d60c2e5de12b88b834b0c2faba8a7067d
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-05-29 19:28:08 -0500

    Added model

--- src/main/java/com/teamaurora/frostburnexpansion/client/render/entity/BriskModel.java
+++ src/main/java/com/teamaurora/frostburnexpansion/client/render/entity/BriskModel.java
@@ -1,14 +1,67 @@
 package com.teamaurora.frostburnexpansion.client.render.entity;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.teamaurora.frostburnexpansion.common.entity.BriskEntity;
+
+import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.SegmentedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class BriskModel<T extends Entity> extends SegmentedModel<T> {
+public class BriskModel<T extends BriskEntity> extends EntityModel<T> {
+
+	private final ModelRenderer head;
+	private final ModelRenderer body;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
+	private final ModelRenderer leg4;
+
+	public BriskModel() {
+		textureWidth = 48;
+		textureHeight = 48;
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(0.0F, 6.0F, 0.0F);
+		head.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+		head.setTextureOffset(0, 32).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.5F, false);
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 6.0F, 0.0F);
+		body.setTextureOffset(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(2.0F, 18.0F, 4.0F);
+		leg1.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(-2.0F, 18.0F, 4.0F);
+		leg2.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(2.0F, 18.0F, -4.0F);
+		leg3.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg4 = new ModelRenderer(this);
+		leg4.setRotationPoint(-2.0F, 18.0F, -4.0F);
+		leg4.setTextureOffset(0, 16).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+	}
 
 	@Override
-	public Iterable<ModelRenderer> getParts() {
-		return null;
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		head.render(matrixStack, buffer, packedLight, packedOverlay);
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg1.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg2.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg3.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg4.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
 	}
 
 	@Override
