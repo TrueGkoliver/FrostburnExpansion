4181d131ee15bc485e05e8a5e36bab6117c135a1
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-05-30 09:27:33 -0500

    Trying to add natural gen

--- src/main/java/com/teamaurora/frostburnexpansion/core/registry/FrostburnExpansionBiomes.java
+++ src/main/java/com/teamaurora/frostburnexpansion/core/registry/FrostburnExpansionBiomes.java
@@ -1,15 +1,20 @@
 package com.teamaurora.frostburnexpansion.core.registry;
 
 import com.teamaurora.frostburnexpansion.core.FrostburnExpansion;
+import com.google.common.graph.ElementOrder.Type;
 import com.teamaurora.frostburnexpansion.common.world.biome.BiomeAuroraOasis;
 
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-
+import static net.minecraftforge.common.BiomeDictionary.Type.*;
 public class FrostburnExpansionBiomes {
 	public static final DeferredRegister<Biome> BIOMES = new DeferredRegister<Biome>(ForgeRegistries.BIOMES, FrostburnExpansion.MODID);
 
@@ -18,7 +23,8 @@ public class FrostburnExpansionBiomes {
 	public static final RegistryObject<BiomeAuroraOasis> AURORA_OASIS = BIOMES.register("aurora_oasis", ()->new BiomeAuroraOasis());
 
 	public static void registerBiomesToDictionary() {
-		// biome dictionary stuff here
+		BiomeManager.addBiome(BiomeType.ICY, new BiomeEntry(AURORA_OASIS.get(), 1));
+		BiomeDictionary.addTypes(AURORA_OASIS.get(), BiomeDictionary.Type.COLD, BiomeDictionary.Type.DRY, BiomeDictionary.Type.RARE);
 	}
 	
 	public static void registerSpawns() {
