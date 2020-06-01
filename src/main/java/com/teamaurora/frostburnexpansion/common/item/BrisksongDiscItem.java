2a3affa365ebe5aa63efb9336d9488979e658521
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-05-31 19:51:45 -0500

    Working on brisksong

---
+++ src/main/java/com/teamaurora/frostburnexpansion/common/item/BrisksongDiscItem.java
@@ -0,0 +1,12 @@
+package com.teamaurora.frostburnexpansion.common.item;
+
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.util.SoundEvent;
+
+public class BrisksongDiscItem extends MusicDiscItem {
+
+	public BrisksongDiscItem(int comparatorValueIn, SoundEvent soundIn, Properties builder) {
+		super(comparatorValueIn, soundIn, builder);
+	}
+
+}
