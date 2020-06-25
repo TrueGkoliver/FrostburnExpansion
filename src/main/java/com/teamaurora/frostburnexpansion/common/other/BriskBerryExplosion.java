65fdb1bd512116d04d1056534ee06268d9180f10
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-06-25 16:26:58 -0500

    Getting access transformer prepped

--- src/main/java/com/teamaurora/frostburnexpansion/common/other/BriskBerryExplosion.java
+++ src/main/java/com/teamaurora/frostburnexpansion/common/other/BriskBerryExplosion.java
@@ -1,13 +1,20 @@
 package com.teamaurora.frostburnexpansion.common.other;
 
+import java.util.List;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.ProtectionEnchantment;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.IFluidState;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
@@ -64,6 +71,50 @@ public class BriskBerryExplosion extends Explosion {
 	               }
 	            }
 	         }
+	         this.affectedBlockPositions.addAll(set);
+	         float f3 = this.size * 2.0F;
+	         int k1 = MathHelper.floor(this.x - (double)f3 - 1.0D);
+	         int l1 = MathHelper.floor(this.x + (double)f3 + 1.0D);
+	         int i2 = MathHelper.floor(this.y - (double)f3 - 1.0D);
+	         int i1 = MathHelper.floor(this.y + (double)f3 + 1.0D);
+	         int j2 = MathHelper.floor(this.z - (double)f3 - 1.0D);
+	         int j1 = MathHelper.floor(this.z + (double)f3 + 1.0D);
+	         List<Entity> list = this.world.getEntitiesWithinAABBExcludingEntity(this.exploder, new AxisAlignedBB((double)k1, (double)i2, (double)j2, (double)l1, (double)i1, (double)j1));
+	         net.minecraftforge.event.ForgeEventFactory.onExplosionDetonate(this.world, this, list, f3);
+	         Vec3d vec3d = new Vec3d(this.x, this.y, this.z);
+
+	         for(int k2 = 0; k2 < list.size(); ++k2) {
+	            Entity entity = list.get(k2);
+	            if (!entity.isImmuneToExplosions()) {
+	               double d12 = (double)(MathHelper.sqrt(entity.getDistanceSq(vec3d)) / f3);
+	               if (d12 <= 1.0D) {
+	                  double d5 = entity.getPosX() - this.x;
+	                  double d7 = entity.getPosYEye() - this.y;
+	                  double d9 = entity.getPosZ() - this.z;
+	                  double d13 = (double)MathHelper.sqrt(d5 * d5 + d7 * d7 + d9 * d9);
+	                  if (d13 != 0.0D) {
+	                     d5 = d5 / d13;
+	                     d7 = d7 / d13;
+	                     d9 = d9 / d13;
+	                     double d14 = (double)getBlockDensity(vec3d, entity);
+	                     double d10 = (1.0D - d12) * d14;
+	                     entity.attackEntityFrom(this.getDamageSource(), (float)((int)((d10 * d10 + d10) / 2.0D * 7.0D * (double)f3 + 1.0D)));
+	                     double d11 = d10;
+	                     if (entity instanceof LivingEntity) {
+	                        d11 = ProtectionEnchantment.getBlastDamageReduction((LivingEntity)entity, d10);
+	                     }
+
+	                     entity.setMotion(entity.getMotion().add(d5 * d11, d7 * d11, d9 * d11));
+	                     if (entity instanceof PlayerEntity) {
+	                        PlayerEntity playerentity = (PlayerEntity)entity;
+	                        if (!playerentity.isSpectator() && (!playerentity.isCreative() || !playerentity.abilities.isFlying)) {
+	                           this.playerKnockbackMap.put(playerentity, new Vec3d(d5 * d10, d7 * d10, d9 * d10));
+	                        }
+	                     }
+	                  }
+	               }
+	            }
+	         
 	}
 
 }
