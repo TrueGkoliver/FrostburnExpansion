00744fd085eebdf5704b62dce4210827652029e2
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-07-06 15:52:30 -0500

    Briskberries now hopefully explode

--- src/main/java/com/teamaurora/frostburnexpansion/common/block/BriskberryBlock.java
+++ src/main/java/com/teamaurora/frostburnexpansion/common/block/BriskberryBlock.java
@@ -2,21 +2,29 @@ package com.teamaurora.frostburnexpansion.common.block;
 
 import java.util.Random;
 
+import com.teamaurora.frostburnexpansion.common.other.BriskBerryExplosion;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BushBlock;
 import net.minecraft.block.IGrowable;
+import net.minecraft.entity.Entity;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
+import javax.annotation.Nullable;
+
 public class BriskberryBlock extends BushBlock implements IGrowable {
 	public static final IntegerProperty AGE = BlockStateProperties.AGE_0_3;
-	public static final IntegerProperty TICKSLEFT = IntegerProperty.create("time_until_explosion", 0, 12000);
+	//SHOULD BE 12000 USUALLY
+	static int temp = 5;
+	public static final IntegerProperty TICKSLEFT = IntegerProperty.create("time_until_explosion", 0, temp);
 	protected BriskberryBlock(Properties properties) {
 		super(properties);
 	}
@@ -42,7 +50,22 @@ public class BriskberryBlock extends BushBlock implements IGrowable {
       if (i>=3) {
       	worldIn.setBlockState(pos, state.with(TICKSLEFT, state.get(TICKSLEFT)-1));
 	  }
+      if (state.get(TICKSLEFT)<=1) {
+      	createExplosion(worldIn, null, null, pos.getX(), pos.getY(), pos.getZ(), 4, false, Explosion.Mode.BREAK);
+	  }
 	}
+	public static Explosion createExplosion(World worldIn, @Nullable Entity entityIn, @Nullable DamageSource damageSourceIn, double xIn, double yIn, double zIn, float explosionRadius, boolean causesFire, Explosion.Mode modeIn) {
+		Explosion explosion = new BriskBerryExplosion(worldIn, entityIn, xIn, yIn, zIn, explosionRadius, causesFire, modeIn);
+		if (damageSourceIn != null) {
+			explosion.setDamageSource(damageSourceIn);
+		}
+		if (net.minecraftforge.event.ForgeEventFactory.onExplosionStart(worldIn, explosion)) return explosion;
+
+		explosion.doExplosionA();
+		explosion.doExplosionB(true);
+		return explosion;
+	}
+
 	@Override
 	public boolean canUseBonemeal(World arg0, Random arg1, BlockPos arg2, BlockState arg3) {
 		return false;
