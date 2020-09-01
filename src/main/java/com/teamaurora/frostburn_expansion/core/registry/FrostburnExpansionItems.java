f8863a5fca44fcedbdba1ddd4f51e4e697fb8a5e
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-01 17:48:19 -0600

    fixed brisksong disc not showing up

--- src/main/java/com/teamaurora/frostburn_expansion/core/registry/FrostburnExpansionItems.java
+++ src/main/java/com/teamaurora/frostburn_expansion/core/registry/FrostburnExpansionItems.java
@@ -18,7 +18,7 @@ public class FrostburnExpansionItems {
 
 	public static final RegistryObject<Item> BRISK_SPAWN_EGG = HELPER.createSpawnEggItem("brisk", ()->FrostburnExpansionEntities.BRISK.get(), 0x84D6DB, 0x359289);
 	public static final RegistryObject<Item> FROZEN_SPORES = HELPER.createItem("frozen_spores", ()->new FrozenSporesItem(new Item.Properties().group(ItemGroup.MATERIALS)));
-	public static final RegistryObject<Item> BRISKSONG_RECORD = HELPER.createItem("music_disc_brisksong", ()->new BrisksongDiscItem(14, FrostburnExpansionSounds.BRISKSONG, new Item.Properties().maxStackSize(1)));
+	public static final RegistryObject<Item> BRISKSONG_RECORD = HELPER.createItem("music_disc_brisksong", ()->new BrisksongDiscItem(14, FrostburnExpansionSounds.BRISKSONG, new Item.Properties().maxStackSize(1).group(ItemGroup.MISC)));
 
 	public static final RegistryObject<Item> AURORAL_BOAT = HELPER.createBoatItem("auroral", FrostburnExpansionBlocks.AURORAL_PLANKS);
 	public static final RegistryObject<Item> OLENYA_BOAT = HELPER.createBoatItem("olenya", FrostburnExpansionBlocks.OLENYA_PLANKS);
