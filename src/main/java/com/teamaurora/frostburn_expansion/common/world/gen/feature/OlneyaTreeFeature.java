0f327cd4ff4776ed3e382aae1b949397097f0a23
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-07 10:48:13 -0600

    added decorator support to olneya

--- src/main/java/com/teamaurora/frostburn_expansion/common/world/gen/feature/OlneyaTreeFeature.java
+++ src/main/java/com/teamaurora/frostburn_expansion/common/world/gen/feature/OlneyaTreeFeature.java
@@ -1,5 +1,6 @@
 package com.teamaurora.frostburn_expansion.common.world.gen.feature;
 
+import com.google.common.collect.Sets;
 import com.mojang.serialization.Codec;
 import com.teamaurora.frostburn_expansion.core.registry.FrostburnExpansionBlocks;
 import net.minecraft.block.Block;
@@ -9,6 +10,8 @@ import net.minecraft.block.LeavesBlock;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldWriter;
@@ -20,7 +23,7 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.structure.StructureManager;
 import net.minecraftforge.common.IPlantable;
 
-import java.util.Random;
+import java.util.*;
 
 public class OlneyaTreeFeature extends Feature<BaseTreeFeatureConfig> {
     public OlneyaTreeFeature(Codec<BaseTreeFeatureConfig> config) {
@@ -80,37 +83,51 @@ public class OlneyaTreeFeature extends Feature<BaseTreeFeatureConfig> {
         if (!flag) {
             return false;
         } else if (isValidGround(worldIn, position.down())) {
+            List<BlockPos> logPos = new ArrayList<>();
+            List<BlockPos> leafPos = new ArrayList<>();
+            setDirtAt(worldIn, position.down());
+
             for (int i=0; i < 1+type; i++) {
-                placeLogAt(worldIn, position.up(i), rand, config);
+                placeLogAt(worldIn, position.up(i), rand, config, logPos);
             }
 
             for (int i=1; i <= plusX; i++) {
-                placeLogAt(worldIn, position.add(i, type+i, 0), rand, config);
+                placeLogAt(worldIn, position.add(i, type+i, 0), rand, config, logPos);
             }
             for (int i=1; i <= minusX; i++) {
-                placeLogAt(worldIn, position.add(-i, type+i, 0), rand, config);
+                placeLogAt(worldIn, position.add(-i, type+i, 0), rand, config, logPos);
             }
             for (int i=1; i <= plusZ; i++) {
-                placeLogAt(worldIn, position.add(0, type+i, i), rand, config);
+                placeLogAt(worldIn, position.add(0, type+i, i), rand, config, logPos);
             }
             for (int i=1; i <= minusZ; i++) {
-                placeLogAt(worldIn, position.add(0, type+i, -i), rand, config);
+                placeLogAt(worldIn, position.add(0, type+i, -i), rand, config, logPos);
             }
 
             if (plusX > 0) {
-                placeCanopyAt(worldIn, position.add(plusX, type+plusX, 0), rand, config);
+                placeCanopyAt(worldIn, position.add(plusX, type+plusX, 0), rand, config, leafPos);
             }
             if (minusX > 0) {
-                placeCanopyAt(worldIn, position.add(-minusX, type+minusX, 0), rand, config);
+                placeCanopyAt(worldIn, position.add(-minusX, type+minusX, 0), rand, config, leafPos);
             }
             if (plusZ > 0) {
-                placeCanopyAt(worldIn, position.add(0, type+plusZ, plusZ), rand, config);
+                placeCanopyAt(worldIn, position.add(0, type+plusZ, plusZ), rand, config, leafPos);
             }
             if (minusZ > 0) {
-                placeCanopyAt(worldIn, position.add(0, type+minusZ, -minusZ), rand, config);
+                placeCanopyAt(worldIn, position.add(0, type+minusZ, -minusZ), rand, config, leafPos);
             }
             if (canopies == 0) {
-                placeCanopyAt(worldIn, position.up(type), rand, config);
+                placeCanopyAt(worldIn, position.up(type), rand, config, leafPos);
+            }
+
+            Set<BlockPos> decSet = Sets.newHashSet();
+            MutableBoundingBox mutableBoundingBox = MutableBoundingBox.getNewBoundingBox();
+            if (!config.decorators.isEmpty()) {
+                logPos.sort(Comparator.comparingInt(Vector3i::getY));
+                leafPos.sort(Comparator.comparingInt(Vector3i::getY));
+                config.decorators.forEach((decorator)->{
+                    decorator.func_225576_a_(worldIn, rand, logPos, leafPos, decSet, mutableBoundingBox);
+                });
             }
 
             return true;
@@ -119,37 +136,37 @@ public class OlneyaTreeFeature extends Feature<BaseTreeFeatureConfig> {
         }
     }
 
-    private void placeCanopyAt(ISeedReader worldIn, BlockPos pos, Random rand, BaseTreeFeatureConfig config) {
-        placeLeafAt(worldIn, pos.add(0, -1, 0), rand, config);
-        placeLeafAt(worldIn, pos.add(-1, -1, 0), rand, config);
-        placeLeafAt(worldIn, pos.add(1, -1, 0), rand, config);
-        placeLeafAt(worldIn, pos.add(0, -1, -1), rand, config);
-        placeLeafAt(worldIn, pos.add(0, -1, 1), rand, config);
+    private void placeCanopyAt(ISeedReader worldIn, BlockPos pos, Random rand, BaseTreeFeatureConfig config, List<BlockPos> leaf) {
+        placeLeafAt(worldIn, pos.add(0, -1, 0), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(-1, -1, 0), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(1, -1, 0), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(0, -1, -1), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(0, -1, 1), rand, config, leaf);
 
         for (int x=-3; x<=3; x++) {
             for (int z=-3; z<=3; z++) {
                 if ((Math.abs(x) == 3 && Math.abs(z) <= 1) || (Math.abs(z) == 3 && Math.abs(x) <= 1)) {
-                    placeRandomLeafAt(worldIn, pos.add(x, 0, z), rand, config, 3);
+                    placeRandomLeafAt(worldIn, pos.add(x, 0, z), rand, config, 3, leaf);
                 } else if (Math.abs(x) != 3 || Math.abs(z) != 3) {
-                    placeLeafAt(worldIn, pos.add(x, 0, z), rand, config);
+                    placeLeafAt(worldIn, pos.add(x, 0, z), rand, config, leaf);
                 }
                 if ((Math.abs(x) == 3 && Math.abs(z) == 2) || (Math.abs(z) == 3 && Math.abs(x) == 2)) {
-                    placeRandomLeafAt(worldIn, pos.add(x, 1, z), rand, config, 2);
+                    placeRandomLeafAt(worldIn, pos.add(x, 1, z), rand, config, 2, leaf);
                 } else if (Math.abs(x) != 3 || Math.abs(z) != 3) {
-                    placeLeafAt(worldIn, pos.add(x, 1, z), rand, config);
+                    placeLeafAt(worldIn, pos.add(x, 1, z), rand, config, leaf);
                 }
             }
         }
 
-        placeLeafAt(worldIn, pos.add(0, 2, 0), rand, config);
-        placeLeafAt(worldIn, pos.add(-1, 2, -1), rand, config);
-        placeLeafAt(worldIn, pos.add(-1, 2, 1), rand, config);
-        placeLeafAt(worldIn, pos.add(1, 2, -1), rand, config);
-        placeLeafAt(worldIn, pos.add(1, 2, 1), rand, config);
-        placeRandomLeafAt(worldIn, pos.add(-1, 2, 0), rand, config, 2);
-        placeRandomLeafAt(worldIn, pos.add(1, 2, 0), rand, config, 2);
-        placeRandomLeafAt(worldIn, pos.add(0, 2, -1), rand, config, 2);
-        placeRandomLeafAt(worldIn, pos.add(0, 2, 1), rand, config, 2);
+        placeLeafAt(worldIn, pos.add(0, 2, 0), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(-1, 2, -1), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(-1, 2, 1), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(1, 2, -1), rand, config, leaf);
+        placeLeafAt(worldIn, pos.add(1, 2, 1), rand, config, leaf);
+        placeRandomLeafAt(worldIn, pos.add(-1, 2, 0), rand, config, 2, leaf);
+        placeRandomLeafAt(worldIn, pos.add(1, 2, 0), rand, config, 2, leaf);
+        placeRandomLeafAt(worldIn, pos.add(0, 2, -1), rand, config, 2, leaf);
+        placeRandomLeafAt(worldIn, pos.add(0, 2, 1), rand, config, 2, leaf);
 
     }
 
@@ -160,22 +177,24 @@ public class OlneyaTreeFeature extends Feature<BaseTreeFeatureConfig> {
         return false;
     }
 
-    private void placeLogAt(IWorldWriter worldIn, BlockPos pos, Random rand, BaseTreeFeatureConfig config)
+    private void placeLogAt(IWorldWriter worldIn, BlockPos pos, Random rand, BaseTreeFeatureConfig config, List<BlockPos> log)
     {
         this.setLogState(worldIn, pos, config.trunkProvider.getBlockState(rand, pos));
+        log.add(pos);
     }
 
-    private void placeRandomLeafAt(IWorldGenerationReader world, BlockPos pos, Random rand, BaseTreeFeatureConfig config, int chance) {
+    private void placeRandomLeafAt(IWorldGenerationReader world, BlockPos pos, Random rand, BaseTreeFeatureConfig config, int chance, List<BlockPos> leaf) {
         if (rand.nextInt(chance) > 0) {
-            placeLeafAt(world, pos, rand, config);
+            placeLeafAt(world, pos, rand, config, leaf);
         }
     }
 
-    private void placeLeafAt(IWorldGenerationReader world, BlockPos pos, Random rand, BaseTreeFeatureConfig config)
+    private void placeLeafAt(IWorldGenerationReader world, BlockPos pos, Random rand, BaseTreeFeatureConfig config, List<BlockPos> leaf)
     {
         if (isAirOrLeaves(world, pos))
         {
             this.setLogState(world, pos, config.leavesProvider.getBlockState(rand, pos).with(LeavesBlock.DISTANCE, 1));
+            leaf.add(pos);
         }
     }
 
